f64fa91d5d72dc9e1ca0a193643cf3a435baac86
Ajijainalmaster <arturrohim16@gmail.com>
2026-03-09 17:50:02 +0700

    Update Install-xray.sh

--- Install-xray.sh
+++ Install-xray.sh
@@ -152,7 +152,7 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/nginx.conf" 
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak && wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/nginx.conf && systemctl restart nginx" 
 #mkdir -p /home/vps/public_html
 wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/vps.conf"
 
