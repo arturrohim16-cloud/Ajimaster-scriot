a2b6fe420561e1cabebfca4627dd27dd27c36854
Ajijainalmaster <arturrohim16@gmail.com>
2026-03-10 15:00:11 +0700

    Update Install-xray.sh

--- Install-xray.sh
+++ Install-xray.sh
@@ -30,6 +30,7 @@ export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
 export Server_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/test"
+export Server1_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/limit"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
@@ -152,7 +153,7 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak && wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/nginx.conf && systemctl restart nginx" 
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/nginx.conf" 
 #mkdir -p /home/vps/public_html
 wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/vps.conf"
 
