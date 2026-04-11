4ab6685b5d60670f34b30995cb9fd660bef4c992
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-11 10:31:35 +0700

    Add header and color variables to netf.sh
    Added header comments and color variables to the script.

--- netf.sh
+++ netf.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : NevermoreSSH
+# (C) Copyright 2022
+# =========================================
+# // Export Color & Information
 Font_Black="\033[30m";
 Font_Red="\033[31m";
 Font_Green="\033[32m";
