3fd57692dce77919b24f229454e2721c1f6d6d7f
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-11 07:54:11 +0700

    Update download source for menu-ss script

--- install.sh
+++ install.sh
@@ -105,7 +105,7 @@ wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/arturrohim16-c
 
 wget -q -O /usr/bin/menu-socks "https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-socks.sh" && chmod +x menu-socks.sh && ./menu-socks.sh
 
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/menu-ss.sh"
+wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-ss.sh"
 
 wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/menu-trojan.sh"
 
