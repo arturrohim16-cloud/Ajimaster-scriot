412c409b499e7b51930847045fd7aa0096c29658
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-11 10:07:03 +0700

    Add script to download and execute Ajimaster scripts
    This script downloads and executes various shell scripts related to the Ajimaster project, including installation and configuration scripts for different protocols.

---
+++ Update-sc.sh
@@ -0,0 +1,75 @@
+wget -q -0 https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-vmess.sh && chmod +x add-vmess.sh && ./add-vmess.sh
+
+wget -q -0 https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-vless.sh && chmod +x add-vless.sh && ./add-vless.sh
+
+wget -q -0 https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-tr.sh && chmod +x add-tr.sh && ./add-tr.sh
+
+wget -q -0 https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-ws.sh && chmod +x add-ws.sh && ./add-ws.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-ssws.sh && chmod +x add-ssws.sh && ./add-ssws.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-socks.sh && chmod +x add-socks.sh && ./add-socks.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/add-trgo.sh && chmod +x add-trgo.sh && ./add-tego.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/autoreboot.sh && chmod +x autoreboot.sh && ./autoreboot.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/restart.sh && chmod +x restart.sh && ./restart.sh
+
+#wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/tendang.sh"
+
+#wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/clearlog.sh"
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/runing.sh && chmod +x runing.sh && ./runing.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/cek-trafik.sh && chmod +x cek-trafik.sg && ./cek-trafik.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/speedtes_cli.py && chmod +x speedtes_cli.py && ./speedtes_cli.py
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/cek-badwing.sh && chmod +x cek-badwing.sh && ./cek-badwing.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/ram.sh && chmod +x ram.sh && ./ram.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/limit-speed.sh && chmod +x limit-speed.sh && ./limit-speed.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-vless.sh && chmod +x menu-vless.sh && ./menu-vless.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-vmess.sh && chmod +x menu-vmess.sh && ./menu-vmess.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-socks.sh && chmod +x menu-socks.sh && ./menu-socks.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-ss.sh && chmod +x menu-ss.sh && ./ menu-ss.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-trojan.sh && chmod +x menu-trojan.sh && ./menu-trojan.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-trgo.sh && chmod +x menu-trgo.sh && ./menu-trgo.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu-ssh.sh && chmod +x menu-ssh.sh && ./menu-ssh
+
+#wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/bekap-tg.sh
+
+#wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/menu-bckp-github.sh
+
+#wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/bckpbot.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/usernew.sh && chmod +x usernew.sh && ./usernew.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu && chmod +x menu.sh && ./menu.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/menu1.sh && chmod +x menu1.sh && ./menu1.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/webbin.sh && chmod +x webbin.sh && ./webbin.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/xp.sh && chmod +x xp.sh && ./xp.sh
+
+#wget -q -O /usr/bin/update "https://raw.githubusercontent.com/arturrohim16-cloud/Blueblue/refs/heads/main/update.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/dns.sh && chmod +x dns.sh && ./dns.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/netf.sh && chmod +x netf.sh && ./netf.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/del-xray.sh && chmod +x del-xray.sh && ./del-xray.sh
+
+wget -q -O https://raw.githubusercontent.com/arturrohim16-cloud/Ajimaster-scriot/refs/heads/main/user-xray.sh && chmod +x user-xray.sh && ./user-xray.sh
