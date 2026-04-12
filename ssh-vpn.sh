b8341027e3782a862c0f018ec4ae6e8e05636736
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-13 02:36:16 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -318,45 +318,49 @@ make install
 cd /root
 rm -rf stunnel5.zip stunnel
 #confik xray.conf
-cat > /etc/nginx/conf.d/xray.conf << 'END'
+cat <<EOF > /etc/nginx/conf.d/xray.conf << END
 server {
     listen 80;
-    listen [::]:80;
     listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name _;
-    root /var/www/html;
-    index index.html;
-
-    # Konfigurasi Path untuk V2RAY / XRAY
-    location /vless {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:14016;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-    }
+    server_name _; # Menggunakan default jika domain bermasalah
+    ssl_certificate /etc/xray/xray.crt;
+    ssl_certificate_key /etc/xray/xray.key;
+    ssl_protocols TLSv1.2 TLSv1.3;
 
     location /vmess {
         proxy_redirect off;
-        proxy_pass http://127.0.0.1:24016;
+        proxy_pass http://127.0.0.1:19146;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
+        proxy_set_header Host \$http_host;
+    }
+
+    location /vless {
+        proxy_pass http://127.0.0.1:24800;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
     }
 
     location /trojan-ws {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:34016;
+        proxy_pass http://127.0.0.1:27723;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
     }
 }
 END
+# Pastikan tidak ada Python yang mengunci port 80 lagi
+fuser -k 80/tcp
+fuser -k 443/tcp
+
+# Cek syntax lagi
+nginx -t
+
+# Jika muncul "syntax is ok", jalankan:
+systemctl restart nginx
+systemctl restart xray
 
 # Config Stunnel5
 mkdir -p /etc/stunnel5
