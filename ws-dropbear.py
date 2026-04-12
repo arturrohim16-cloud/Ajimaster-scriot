03f8fea0cf0b8396fc08ba2ed22908e5bc0a0f37
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-12 17:11:12 +0700

    Simplify RESPONSE variable in ws-dropbear.py

--- ws-dropbear.py
+++ ws-dropbear.py
@@ -16,16 +16,8 @@ BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:109'
 # Copy dan paste kode ini untuk menggantikan bagian RESPONSE di script Anda
-RESPONSE = b'HTTP/1.1 200 Websocket_openvpn\r\nContent-length: 0\r\nHTTP/1.1 101 Switching Protocols\r\n\r\n'
-RESPONSE += b'HTTP/1.1 301 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n\r\n'
-RESPONSE += b'HTTP/1.1 69 Connection Established\r\n\r\n'
-RESPONSE += b'HTTP/1.1 101 Switching Protocols\r\n' \
-           b'Upgrade: websocket\r\n' \
-           b'Connection: Upgrade\r\n' \
-           b'Sec-WebSocket-Accept: foo\r\n' \
-           b'Server: nginx/1.18.0\r\n' \
-           b'Content-Type: application/octet-stream\r\n' \
-           b'\r\n'
+RESPONSE = b'HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\nServer: nginx/1.18.0\r\nContent-Type: application/octet-stream\r\n\r\n'
+
 class Server(threading.Thread):
     def __init__(self, host, port):
         threading.Thread.__init__(self)
