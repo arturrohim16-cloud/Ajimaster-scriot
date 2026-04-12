f7654fe3de70f305dd2e8647237b36b18a7b2cfd
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-12 23:10:21 +0700

    Include IP and global limits in log output
    Added IP and global limit information to user log.

--- add-ws.sh
+++ add-ws.sh
@@ -305,6 +305,8 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "Link Vmess Config : http://${domain}:81/vmess-$user.txt" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Expired On : $exp" | tee -a /etc/log-create-user.log
+echo -e "limit ip : $iplimit"
+echo -e "limit gn : $gblimit"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
 rm /etc/xray/$user-tls.json > /dev/null 2>&1
