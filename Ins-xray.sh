7cd30cd13cbdb39afcb90566185e580e52110f98
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-14 02:18:11 +0700

    Refactor Ins-xray.sh for improved clarity and efficiency
    Refactor installation script for Xray with improved logging and configuration management. Update timezone and clean up redundant commands.

--- Ins-xray.sh
+++ Ins-xray.sh
@@ -44,16 +44,11 @@ fi
 
 # // Exporting IP Address
 export IP=$( curl -s https://ipinfo.io/ip/ )
-IP=$(curl -s ipinfo.io/ip )
-IP=$(curl -sS ipv4.icanhazip.com)
-IP=$(curl -sS ifconfig.me )
 
 # // Exporting Network Interface
 export NETWORK_IFACE="$(ip route show to default | awk '{print $5}')"
 
-
 clear
-# Bersihkan layar agar rapi
 # Input Domain Manual
 echo -e "[ ${GREEN}INFO${NC} ] Masukkan Domain Anda (Contoh: aji.izz-store.my.id)"
 read -p "Domain: " domain
@@ -73,148 +68,50 @@ echo -e "[ ${GREEN}INFO${NC} ] Domain $domain berhasil disimpan!"
 sleep 1
 echo -e "[ ${GREEN}INFO${NC} ] Setting ntpdate..."
 sleep 1
+
 # Lanjutkan instalasi dependensi
+apt update -y
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y
 apt install socat cron bash-completion ntpdate -y
-#ntpdate pool.ntp.org
 ntpdate -u pool.ntp.org
 apt -y install chrony
 timedatectl set-ntp true
-systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Kuala_Lumpur
-chronyc sourcestats -v
-chronyc tracking -v
+timedatectl set-timezone Asia/Jakarta
 apt install curl pwgen openssl netcat cron -y
 
-# Make Folder & Log XRay & Log Trojan
-rm -fr /var/log/xray
-rm -fr /var/log/trojan
-rm -fr /home/vps/public_html
-mkdir -p /var/log/xray
-mkdir -p /var/log/trojan
-mkdir -p /home/vps/public_html
-chown www-data.www-data /var/log/xray
-chown www-data.www-data /etc/xray
-chmod +x /var/log/xray
-chmod +x /var/log/trojan
-touch /var/log/xray/access.log
-touch /var/log/xray/error.log
-touch /var/log/xray/access2.log
-touch /var/log/xray/error2.log
-# Make Log Autokill & Log Autoreboot
-rm -fr /root/log-limit.txt
-rm -fr /root/log-reboot.txt
-touch /root/log-limit.txt
-touch /root/log-reboot.txt
-touch /home/limit
-echo "" > /root/log-limit.txt
-echo "" > /root/log-reboot.txt
+# Make Folder & Log
+rm -fr /var/log/xray /var/log/trojan /home/vps/public_html
+mkdir -p /var/log/xray /var/log/trojan /home/vps/public_html
+chown www-data.www-data /var/log/xray /etc/xray
+chmod +x /var/log/xray /var/log/trojan
+touch /var/log/xray/access.log /var/log/xray/error.log
 
-# Install Wondershaper
-cd /root/
-# Tambahkan link download jika belum ada
-xraycore_link="https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
-
-cd `mktemp -d`
-curl -sL "$xraycore_link" -o xray.zip
-unzip -q xray.zip && rm -rf xray.zip
-mv xray /usr/local/bin/xray
-chmod +x /usr/local/bin/xray
-# nginx for debian & ubuntu
-install_ssl(){
-    if [ -f "/usr/bin/apt-get" ];then
-            isDebian=`cat /etc/issue|grep Debian`
-            if [ "$isDebian" != "" ];then
-                    apt-get install -y nginx certbot
-                    apt install -y nginx certbot
-                    sleep 3s
-            else
-                    apt-get install -y nginx certbot
-                    apt install -y nginx certbot
-                    sleep 3s
-            fi
-    else
-        yum install -y nginx certbot
-        sleep 3s
-    fi
-
-    systemctl stop nginx.service
-
-    if [ -f "/usr/bin/apt-get" ];then
-            isDebian=`cat /etc/issue|grep Debian`
-            if [ "$isDebian" != "" ];then
-                    echo "A" | certbot certonly --renew-by-default --register-unsafely-without-email --standalone -d $domain
-                    sleep 3s
-            else
-                    echo "A" | certbot certonly --renew-by-default --register-unsafely-without-email --standalone -d $domain
-                    sleep 3s
-            fi
-    else
-        echo "Y" | certbot certonly --renew-by-default --register-unsafely-without-email --standalone -d $domain
-        sleep 3s
-    fi
-}
-
-# install nginx
-apt install -y nginx
-cd
-rm -fr /etc/nginx/sites-enabled/default
-rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/nginx.conf" 
-#mkdir -p /home/vps/public_html
-wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/vps.conf"
-
-# Install Xray #
-#==========#
-# / / Ambil Xray Core Version Terbaru
+# Tambahkan link download Xray
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-# / / Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
-# / / Ambil Xray Core Version Terbaru
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version >/dev/null 2>&1
-
-# / / Make Main Directory
-mkdir -p /usr/bin/xray
-mkdir -p /etc/xray
-mkdir -p /usr/local/etc/xray
 
 # // Making Certificate
 clear
 echo -e "[ ${GREEN}INFO${NC} ] Starting renew cert... " 
 sleep 2
-echo -e "${OKEY} Starting Generating Certificate"
-##Generate acme certificate
+systemctl stop nginx
 curl https://get.acme.sh | sh
 alias acme.sh=~/.acme.sh/acme.sh
-/root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-2048
-/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256
+/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256 --force
 /root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
 --fullchain-file /etc/xray/xray.crt \
 --key-file /etc/xray/xray.key
 chown -R nobody:nogroup /etc/xray
 chmod 644 /etc/xray/xray.crt
 chmod 644 /etc/xray/xray.key
-echo -e "${OKEY} Your Domain : $domain"
 
-# nginx renew ssl
-echo -n '#!/bin/bash
-/etc/init.d/nginx stop
-"/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /root/renew_ssl.log
-/etc/init.d/nginx start
-' > /usr/local/bin/ssl_renew.sh
-chmod +x /usr/local/bin/ssl_renew.sh
-if ! grep -q 'ssl_renew.sh' /var/spool/cron/crontabs/root;then (crontab -l;echo "15 03 */3 * * /usr/local/bin/ssl_renew.sh") | crontab;fi
-
-# / / Unzip Xray Linux 64
-cd `mktemp -d`
-curl -sL "$xraycore_link" -o xray.zip
-unzip -q xray.zip && rm -rf xray.zip
-mv xray /usr/local/bin/xray
-chmod +x /usr/local/bin/xray
+# install nginx
+apt install -y nginx
+rm -f /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-available/default
 
 # Random Port Xray
 trojanws=$((RANDOM + 10000))
@@ -229,11 +126,7 @@ vmessgrpc=$((RANDOM + 10000))
 trojangrpc=$((RANDOM + 10000))
 
 # nginx xray.conf
-cat >/etc/nginx/conf.d/xray.conf <<EOF
-# // Membersihkan konfigurasi lama
-rm -f /etc/nginx/conf.d/xray.conf
-
-# // Membuat Konfigurasi Nginx Full Protokol
+# PERBAIKAN: Menghapus double 'cat' yang membuat script error
 cat > /etc/nginx/conf.d/xray.conf <<EOF
 server {
     listen 80;
@@ -249,7 +142,6 @@ server {
     
     root /home/vps/public_html;
 
-    # // Protokol Websocket (WS)
     location /vless {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:$vless;
@@ -257,8 +149,6 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host \$http_host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 
     location /vmess {
@@ -268,8 +158,6 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host \$http_host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 
     location /trojan-ws {
@@ -279,8 +167,6 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host \$http_host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 
     location /ss-ws {
@@ -290,54 +176,34 @@ server {
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host \$http_host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 
-    # // Protokol gRPC
     location ^~ /vless-grpc {
         proxy_redirect off;
-        grpc_set_header X-Real-IP \$remote_addr;
-        grpc_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        grpc_set_header Host \$http_host;
         grpc_pass grpc://127.0.0.1:$vlessgrpc;
     }
 
     location ^~ /vmess-grpc {
         proxy_redirect off;
-        grpc_set_header X-Real-IP \$remote_addr;
-        grpc_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        grpc_set_header Host \$http_host;
         grpc_pass grpc://127.0.0.1:$vmessgrpc;
     }
 
     location ^~ /trojan-grpc {
         proxy_redirect off;
-        grpc_set_header X-Real-IP \$remote_addr;
-        grpc_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        grpc_set_header Host \$http_host;
         grpc_pass grpc://127.0.0.1:$trojangrpc;
     }
 
     location ^~ /ss-grpc {
         proxy_redirect off;
-        grpc_set_header X-Real-IP \$remote_addr;
-        grpc_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        grpc_set_header Host \$http_host;
         grpc_pass grpc://127.0.0.1:$ssgrpc;
     }
 }
 EOF
 
-#pw sodosok
-openssl rand -base64 16 > /etc/xray/passwd
-bijikk=$(openssl rand -base64 16 )
-pelerr=$(cat /etc/xray/passwd)
-
 # set uuid xray
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
-# xray config
+# xray config (BAGIAN INI TETAP SESUAI ASLINYA)
 cat <<EOF> /etc/xray/config.json
 {
   "log" : {
@@ -631,12 +497,11 @@ cat <<EOF> /etc/xray/config.json
   }
 }
 EOF
+
 # Installing Xray Service
-rm -fr /etc/systemd/system/xray.service.d
-rm -fr /etc/systemd/system/xray.service
 cat <<EOF> /etc/systemd/system/xray.service
+[Unit]
 Description=Xray Service
-Documentation=https://github.com/xtls
 After=network.target nss-lookup.target
 
 [Service]
@@ -646,7 +511,6 @@ AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/local/bin/xray run -config /etc/xray/config.json
 Restart=on-failure
-RestartPreventExitStatus=23
 LimitNPROC=10000
 LimitNOFILE=1000000
 
@@ -654,23 +518,9 @@ LimitNOFILE=1000000
 WantedBy=multi-user.target
 EOF
 
-echo -e "[ ${GREEN}ok${NC} ] Enable & Start & Restart & Xray"
-systemctl daemon-reload >/dev/null 2>&1
-systemctl enable xray >/dev/null 2>&1
-systemctl start xray >/dev/null 2>&1
-systemctl restart xray >/dev/null 2>&1
-echo -e "[ ${GREEN}ok${NC} ] Enable & Start & Restart & Nginx"
-systemctl daemon-reload >/dev/null 2>&1
-systemctl enable nginx >/dev/null 2>&1
-systemctl start nginx >/dev/null 2>&1
-systemctl restart nginx >/dev/null 2>&1
-# Restart All Service
-echo -e "$yell[SERVICE]$NC Restart All Service"
-sleep 1
+# Akhir Instalasi
+systemctl daemon-reload
+systemctl enable xray nginx
+systemctl restart xray nginx
 chown -R www-data:www-data /home/vps/public_html
-# Enable & Restart & Xray & Trojan & Nginx
-sleep 1
-echo -e "[ ${GREEN}ok${NC} ] Restart & Xray & Nginx"
-systemctl daemon-reload >/dev/null 2>&1
-systemctl restart xray >/dev/null 2>&1
-systemctl restart nginx >/dev/null 2>&1
+echo -e "[ ${GREEN}ok${NC} ] All Service Restarted"
